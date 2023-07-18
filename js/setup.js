7745c3544e7f9a5fe2c0e303b5dca8aba75d039e
adriana <adrianapiccolo00@gmail.com>
2023-07-18 16:25:24 +0200

    done some

---
+++ js/setup.js
@@ -0,0 +1,33 @@
+let pokeArray = [];
+
+async function getAllPokemons() {
+  let pokeObj = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=898")
+    .then((response) => response.json())
+    .then((result) => result.results)
+    .catch((error) => console.log("error", error));
+
+  pokeObj.forEach((poke) => {
+    fetch(poke.url)
+      .then((response) => response.json())
+      .then((result) => pokeArray.push(result))
+      .catch((error) => console.log("error", error));
+  });
+
+  setTimeout(() => {
+    loadingCompletion();
+  }, 1000);
+}
+
+function loadingCompletion() {
+  const loadingDiv = document.getElementById("loading-div");
+  loadingDiv.classList.add("hideLoading");
+
+  setTimeout(function () {
+    loadingDiv.classList.replace("hideLoading", "hide");
+    document.body.style.overflow = "unset";
+  }, 500);
+  
+  currentList = pokeArray.sort((a, b) => a.id - b.id);
+  
+  updatePokemonList();
+}
